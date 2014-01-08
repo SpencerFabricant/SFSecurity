011fac7dfa4bc54113f0dc2998278b49c5290495
SpencerFabricant <Spencer.Fabricant@gmail.com>
2014-01-08 15:56:33 -0500

    we got motion detection

--- playground/TestWebcam.java
+++ playground/TestWebcam.java
@@ -3,7 +3,7 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import com.github.sarxos.webcam.Webcam;
-
+import sfsecurity.util.Snapshot;
 
 public class TestWebcam {
 	public static void main(String[] args) throws IOException {
@@ -13,13 +13,20 @@ public class TestWebcam {
 		webcam.open();
 		System.out.println("somethingsomething...");
 		BufferedImage image = webcam.getImage();
-		image = webcam.getImage();
+		Snapshot s = new Snapshot(image);
+		Snapshot s2;
 		System.out.println("...");
-		int counter = 0;
-		while(counter < 300){
-			image = webcam.getImage();
-			ImageIO.write(image, "JPG", new File("files/test" + (counter++) + ".jpg"));
+		for (int i=0;i<300;i++) {
+			s2 = new Snapshot(webcam.getImage());
+			System.out.println(s.compare(s2));
+			s = s2;
+//			ImageIO.write(s.smallImage, "JPG", new File("tmpfiles/image"+i+".jpg"));
 		}
+//		int counter = 0;
+//		while(counter < 300){
+//			image = webcam.getImage();
+//			ImageIO.write(image, "JPG", new File("files/test" + (counter++) + ".jpg"));
+//		}
 		System.out.println("DONE");
 	}
 }
--- src/sfsecurity/util/Snapshot.java
+++ src/sfsecurity/util/Snapshot.java
@@ -1,23 +1,46 @@
 package sfsecurity.util;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Date;
 
 
+/**
+ * Snapshot:  Contain
+ * @author Spencer Fabricant
+ */
 public class Snapshot {
-	private final int threshold;
+	private static final int threshold = 20;
+	private static final int SMALL_SIZE = 16;
+	public final Date timestamp;
 	public final BufferedImage image;
 	public final BufferedImage smallImage;
 	public Snapshot(BufferedImage image) {
-		threshold = 20;
+		timestamp = new Date();
 		this.image = image;
-		this.smallImage = image; // TODO: resize
+		smallImage = new BufferedImage(SMALL_SIZE, SMALL_SIZE, BufferedImage.TYPE_BYTE_GRAY);
+		Graphics g = smallImage.createGraphics();
+		g.drawImage(image, 0, 0, SMALL_SIZE, SMALL_SIZE, null);
+		g.dispose();
 	}
 	public boolean detectMotion(Snapshot snapshot) {
 		// TODO
 		if (compare(snapshot) >= threshold) return true;
 		return false;
 	}
-	private int compare(Snapshot snapshot) {
-		return 0; // TODO
+	/**
+	 * returns average sum sqared difference per pixel of
+	 * reduced-sized greyscale image
+	 */
+	public double compare(Snapshot snapshot) {
+		double total = 0;
+		for (int i=0; i<SMALL_SIZE; i++) {
+			for (int j=0; j<SMALL_SIZE; j++) {
+				int diff = (this.smallImage.getRGB(i,j)&0xFF)
+						- (snapshot.smallImage.getRGB(i, j)&0xFF);
+				total += (diff*diff);
+			}
+		}
+		return (total/(SMALL_SIZE * SMALL_SIZE));
 	}
 }
